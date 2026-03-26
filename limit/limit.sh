6aa956c1665cf57b57dcf66fe3ac389053a73f98
M DZULFIKRI ALIFAJRI <23041450155@radenfatah.ac.id>
2026-03-26 10:21:16 -0700

    Refactor configuration and service files for improved functionality and organization
    - Updated `config.json` to streamline outbound settings and enhance API service configuration.
    - Modified `dropbear.conf` to change the banner file path for client connection messages.
    - Cleaned up `haproxy.cfg` by removing unnecessary configurations and adding new comments for clarity.
    - Enhanced `issue.net` with a welcome message and usage rules for SSH Premium.
    - Refined `limit-ip` script to improve IP limit handling and logging.
    - Updated `limit.sh` to change the repository source for service files and scripts.
    - Changed documentation links in service files (`limitshadowsocks.service`, `limittrojan.service`, `limitvless.service`, `limitvmess.service`) to point to the new GitHub repository.
    - Adjusted `openvpn` script to update the source of the VPN zip file.
    - Improved `quota` script to dynamically fetch bot credentials from a configuration file.
    - Updated `rclone.conf` with a new access token and root folder ID.
    - Modified `runn.service` to change service type and restart behavior.
    - Adjusted `shadowsocks` script to correct file paths for user removal.
    - Updated service descriptions and documentation links in `socks.service`, `udp-mini-1.service`, `udp-mini-2.service`, `udp-mini-3.service`, and `ws.service`.
    - Refined `xray.conf` to update server names and improve request handling logic.

--- limit/limit.sh
+++ limit/limit.sh
@@ -1,12 +1,13 @@
-REPO="https://raw.githubusercontent.com/dalifajr/panelxray/main/"
-wget -q -O /etc/systemd/system/limitvmess.service "${REPO}limit/limitvmess.service" && chmod +x /etc/systemd/system/limitvmess.service >/dev/null 2>&1
-wget -q -O /etc/systemd/system/limitvless.service "${REPO}limit/limitvless.service" && chmod +x /etc/systemd/system/limitvless.service >/dev/null 2>&1
-wget -q -O /etc/systemd/system/limittrojan.service "${REPO}limit/limittrojan.service" && chmod +x /etc/systemd/system/limittrojan.service >/dev/null 2>&1
-wget -q -O /etc/systemd/system/limitshadowsocks.service "${REPO}limit/limitshadowsocks.service" && chmod +x /etc/systemd/system/limitshadowsocks.service >/dev/null 2>&1
-wget -q -O /etc/xray/limit.vmess "${REPO}limit/vmess" >/dev/null 2>&1
-wget -q -O /etc/xray/limit.vless "${REPO}limit/vless" >/dev/null 2>&1
-wget -q -O /etc/xray/limit.trojan "${REPO}limit/trojan" >/dev/null 2>&1
-wget -q -O /etc/xray/limit.shadowsocks "${REPO}limit/shadowsocks" >/dev/null 2>&1
+#!/bin/bash
+REPO="https://raw.githubusercontent.com/Arya-Blitar22/st-pusat/main/"
+wget -q -O /etc/systemd/system/limitvmess.service "${REPO}media/limitvmess.service" && chmod +x limitvmess.service >/dev/null 2>&1
+wget -q -O /etc/systemd/system/limitvless.service "${REPO}media/limitvless.service" && chmod +x limitvless.service >/dev/null 2>&1
+wget -q -O /etc/systemd/system/limittrojan.service "${REPO}media/limittrojan.service" && chmod +x limittrojan.service >/dev/null 2>&1
+wget -q -O /etc/systemd/system/limitshadowsocks.service "${REPO}media/limitshadowsocks.service" && chmod +x limitshadowsocks.service >/dev/null 2>&1
+wget -q -O /etc/xray/limit.vmess "${REPO}media/vmess" >/dev/null 2>&1
+wget -q -O /etc/xray/limit.vless "${REPO}media/vless" >/dev/null 2>&1
+wget -q -O /etc/xray/limit.trojan "${REPO}media/trojan" >/dev/null 2>&1
+wget -q -O /etc/xray/limit.shadowsocks "${REPO}media/shadowsocks" >/dev/null 2>&1
 chmod +x /etc/xray/limit.vmess
 chmod +x /etc/xray/limit.vless
 chmod +x /etc/xray/limit.trojan
@@ -16,11 +17,3 @@ systemctl enable --now limitvmess
 systemctl enable --now limitvless
 systemctl enable --now limittrojan
 systemctl enable --now limitshadowsocks
-# systemctl start limitvmess
-# systemctl start limitvless
-# systemctl start limittrojan
-# systemctl start limitshadowsocks
-# systemctl restart limitvmess
-# systemctl restart limitvless
-# systemctl restart limittrojan
-# systemctl restart limitshadowsocks
